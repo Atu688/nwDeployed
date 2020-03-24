011a2cfb5276a16108e58d533fa5e604b42bac53
nonwill <nonwill@hotmail.com>
2020-03-24 19:17:28 +0800

    tesseract4 test ok

---
+++ test_tesseract.bat
@@ -0,0 +1,5 @@
+start .\bin5_x64_msvc\tesseract4.exe images\feyn.gif feyn.gif --tessdata-dir ./tessdata_best -l eng -c tessedit_create_pdf=1
+start .\bin5_x64_msvc\tesseract4.exe images\feyn.webp feyn.webp --tessdata-dir ./tessdata_best -l eng -c tessedit_create_pdf=1
+start .\bin5_x64_msvc\tesseract4.exe images\feyn.tif feyn.tif --tessdata-dir ./tessdata_best -l eng -c tessedit_create_pdf=1
+start .\bin5_x64_msvc\tesseract4.exe images\feyn.png feyn.png --tessdata-dir ./tessdata_best -l eng -c tessedit_create_pdf=1
+start .\bin5_x64_msvc\tesseract4.exe images\feyn.jpg feyn.jpg --tessdata-dir ./tessdata_best -l eng -c tessedit_create_pdf=1
